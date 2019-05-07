4e2b66c15464ad3ec8067d31458bac42de3d3ee3
Peter Uithoven <peter@peteruithoven.nl>
2019-05-07 14:33:24 +0200

    GraphQL experiments

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const gql = require('graphql-tag');
+const ApolloClient = require('apollo-boost').ApolloClient;
+const fetch = require('cross-fetch/polyfill').fetch;
+const createHttpLink = require('apollo-link-http').createHttpLink;
+const InMemoryCache = require('apollo-cache-inmemory').InMemoryCache;
+const setContext = require('apollo-link-context').setContext;
+require('dotenv').config();
+
+const httpLink = createHttpLink({
+    uri: "https://api.github.com/graphql/v4",
+    fetch: fetch
+});
+
+const token = process.env.REACT_APP_GITHUB_TOKEN;
+console.log('token: ', token);
+
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query {
+        viewer {
+          login
+        }
+      }
+    `
+  })
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
--- src/index.js
+++ src/index.js
@@ -4,7 +4,44 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient, { gql } from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const token = process.env.REACT_APP_GITHUB_TOKEN;
+console.log('token: ', token);
+
+const client = new ApolloClient({
+  // uri: "https://48p1r2roz4.sse.codesandbox.io",
+  uri: "https://api.github.com/graphql/v4",
+  // uri: "/graphql/v4/",
+  request: async operation => {
+    // const token = localStorage.getItem('token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+   }
+});
+
+
+client
+  .query({
+    query: gql`
+      query {
+        viewer {
+          login
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+ReactDOM.render((
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>)
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
