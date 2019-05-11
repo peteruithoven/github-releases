23e4994ba6a39036f13ad5975eefb6feb60b3ceb
Peter Uithoven <peter@peteruithoven.nl>
2019-05-12 00:25:48 +0200

    Abstracted AppBarSelector

---
+++ src/components/AppBarSelect.js
@@ -0,0 +1,63 @@
+import React from "react";
+import dayjs from "dayjs";
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => {
+    const { palette } = theme;
+    const selectColor = palette.getContrastText(palette.text.primary);
+    const selectColorValue = `${selectColor} !important`;
+    return {
+        select: {
+            color: selectColorValue,
+            '&:before': {
+                borderColor: selectColorValue,
+            },
+            '&:after': {
+                borderColor: selectColorValue,
+            }
+        },
+        icon: {
+            fill: selectColorValue,
+        },
+        light: {
+            color: selectColorValue
+        }
+    }
+};
+
+const AppBarSelect = ({ id, label, value, options, onChange, classes }) => (
+    <form>
+        <FormControl>
+            <InputLabel
+                shrink
+                htmlFor={id}
+                className={classes.light}
+            >
+                {label}
+            </InputLabel>
+            <Select
+                value={value}
+                onChange={event => onChange(event.target.value)}
+                className={classes.select}
+                inputProps={{
+                    name:{id},
+                    id:{id},
+                    classes: {
+                        icon: classes.icon,
+                    }
+                }}
+            >
+                {options.map(({ value, content }) => (
+                    <MenuItem value={value} key={value}>
+                        { content }
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+    </form>
+)
+export default withStyles(styles, { withTheme: true })(AppBarSelect);
--- src/components/MonthsSelector.js
+++ src/components/MonthsSelector.js
@@ -1,63 +1,19 @@
 import React from "react";
 import dayjs from "dayjs";
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import { withStyles } from '@material-ui/core/styles';
+import AppBarSelect from './AppBarSelect.js';
 
-const styles = theme => {
-    const { palette } = theme;
-    const selectColor = palette.getContrastText(palette.text.primary);
-    const selectColorValue = `${selectColor} !important`;
-    return {
-        select: {
-            color: selectColorValue,
-            '&:before': {
-                borderColor: selectColorValue,
-            },
-            '&:after': {
-                borderColor: selectColorValue,
-            }
-        },
-        icon: {
-            fill: selectColorValue,
-        },
-        light: {
-            color: selectColorValue
-        }
-    }
-};
-
-const MonthsSelector = ({ month, months, onChange, classes }) => (
+const MonthsSelector = ({ month, months, onChange }) => (
     <form>
-        <FormControl>
-            <InputLabel
-                shrink
-                htmlFor="month"
-                className={classes.light}
-            >
-                Within
-            </InputLabel>
-            <Select
-                value={month}
-                onChange={event => onChange(event.target.value)}
-                className={classes.select}
-                inputProps={{
-                    name:"month",
-                    id:"month",
-                    classes: {
-                        icon: classes.icon,
-                    }
-                }}
-            >
-                {months.map(month => (
-                    <MenuItem value={month} key={month}>
-                        {dayjs(month).format('MMM YYYY')}
-                    </MenuItem>
-                ))}
-            </Select>
-        </FormControl>
+        <AppBarSelect
+            id="month"
+            label="Within"
+            value={month}
+            options={months.map(month => ({
+                value: month,
+                content: dayjs(month).format('MMM YYYY')
+            }))}
+            onChange={onChange}
+        />
     </form>
 )
-export default withStyles(styles, { withTheme: true })(MonthsSelector);
+export default MonthsSelector;
