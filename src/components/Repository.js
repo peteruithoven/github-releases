e0d7f6cc56bb3321472a6243865025712ef36dfc
Peter Uithoven <peter@peteruithoven.nl>
2019-06-20 23:33:52 +0200

    Spacing tweaks

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -1,30 +1,28 @@
 import React from 'react';
-import {
-  Box,
-  Card,
-  CardContent,
-  List,
-  Link,
-  Typography,
-} from '@material-ui/core';
+import { Box, Card, List, Link, Typography } from '@material-ui/core';
+import { styled } from '@material-ui/core/styles';
 import Release from './Release.js';
+
+const StyledList = styled(List)({
+  paddingTop: 0,
+});
 
 const Repository = ({ data }) => (
   <Box m={[2, 3]} clone>
     <Card>
-      <CardContent>
+      <Box pt={1} px={2}>
         <Typography gutterBottom variant="h4" component="h2">
           <Link href={data.url}>{data.name}</Link>
         </Typography>
         <Typography>
           <Link href={data.compareURL}>View all changes</Link>
         </Typography>
-        <List>
+        <StyledList>
           {data.releases.reverse().map(release => (
             <Release data={release} key={release.id} />
           ))}
-        </List>
-      </CardContent>
+        </StyledList>
+      </Box>
     </Card>
   </Box>
 );
